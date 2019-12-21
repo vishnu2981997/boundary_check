08e568af6888230299d6cb3dc69c065c4af4c61c
vishnu2981997 <vishnu2981997@gmail.com>
2019-12-21 22:19:53 +0530

    updated urls and added gitignore

--- utils.py
+++ utils.py
@@ -9,21 +9,6 @@ class BoundaryCheck:
         self._coords = coords
 
     @staticmethod
-    def centroid(coords):
-        """
-        calculate centroid
-        """
-        x_list = [i[0] for i in coords]
-        y_list = [i[1] for i in coords]
-
-        length = len(coords)
-
-        x = sum(x_list) / length
-        y = sum(y_list) / length
-
-        return x, y
-
-    @staticmethod
     def area_of_triangle(x1, y1, x2, y2, x3, y3):
         """
         calculate area of triangle
@@ -32,13 +17,13 @@ class BoundaryCheck:
 
         return area
 
-    def area_of_polygon(self, centroid):
+    def area_of_polygon_wrt_point(self, centroid):
         """
         calculate area of polygon
         """
         x, y = [i for i in centroid]
 
-        area_of_polygon = 0
+        area_of_polygon = 0.0
 
         for i in range(len(self._coords) - 1):
             x1, y1 = [i for i in self._coords[i]]
@@ -52,14 +37,25 @@ class BoundaryCheck:
 
         return area_of_polygon
 
+    def area_of_polygon(self):
+        x = [i[0] for i in self._coords]
+        y = [i[1] for i in self._coords]
+        n = len(self._coords)
+        area = 0.0
+        j = n - 1
+        for i in range(0, n):
+            area += (x[j] + x[i]) * (y[j] - y[i])
+            j = i
+        return int(abs(area / 2.0))
+
     def exists(self, point):
         """
         check if the the given point exists within the rectangle or not
         """
-        x, y = self.centroid(coords=self._coords)
-        area_with_centroid = self.area_of_polygon([x, y])
+
+        area_with_centroid = self.area_of_polygon()
 
         x, y = [i for i in point]
-        area_with_new_point = self.area_of_polygon([x, y])
+        area_with_new_point = self.area_of_polygon_wrt_point([x, y])
 
         return area_with_centroid == area_with_new_point
